d962959c469944c5b056e33feb229c27b5e1a97f
SimoPrG <simeon_petev@hotmail.com>
2015-03-24 17:41:39 +0200

    Console project added to the solution and KeyboardInput implements IUserInput

---
+++ EasterFarm/EasterFarm.GameLogic/Contracts/IUserInput.cs
@@ -0,0 +1,23 @@
+﻿namespace EasterFarm.GameLogic.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IUserInput
+    {
+        event EventHandler OnLeftPressed;
+
+        event EventHandler OnRightPressed;
+
+        event EventHandler OnUpPressed;
+
+        event EventHandler OnDownPressed;
+
+        event EventHandler OnActionPressed;
+
+        void ProcessInput();
+    }
+}
