a12a5a4910aed80d153eb14e8c27ae1486715469
svetlai <svetla_22@yahoo.com>
2015-03-24 21:41:00 +0200

    Custom exception added: InsufficientAmmountException.

---
+++ EasterFarm/EasterFarm.Common/InsufficientAmmountException.cs
@@ -0,0 +1,41 @@
+﻿namespace EasterFarm.Common
+{
+    using System;
+
+    public class InsufficientAmmountException : Exception
+    {
+        private string item;
+
+        public InsufficientAmmountException(string message, string item)
+            : this(message, null, item)
+        {
+        }
+
+        public InsufficientAmmountException(string message, Exception innerException, string item)
+            : base(message, innerException)
+        {
+            this.Item = item;
+        }
+
+        public string Item
+        {
+            get
+            {
+                return this.item;
+            }
+
+            private set
+            {
+                this.item = value;
+            }
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Insufficient ammount. You don't have enough of item <<{0}>>.", this.Item);
+            }
+        }
+    }
+}
