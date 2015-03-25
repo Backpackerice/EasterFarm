61d4b6560d31e5cfd927aca2457a4b0601ed3f30
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-25 13:44:32 +0200

    GameInitializator and Engine in progress

--- EasterFarm/EasterFarm.GameLogic/GameInitializator.cs
+++ EasterFarm/EasterFarm.GameLogic/GameInitializator.cs
@@ -6,8 +6,16 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class GameInitializator
-    {
+    using EasterFarm.Models;
+    using EasterFarm.Models.FarmObjects.Food;
 
+    public static class GameInitializator
+    {
+        public static void Initialize(Engine engine)
+        {
+            GameObject Raspberry = new Raspberry(new MatrixCoords(3, 4), new char[,] {{'R'}}, false);
+
+            engine.AddGameObject(Raspberry);
+        }
     }
 }
