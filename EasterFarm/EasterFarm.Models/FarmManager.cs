4013eb3ae17140b3006c51e658210e5e00e775c1
svetlai <svetla_22@yahoo.com>
2015-03-25 13:42:03 +0200

    PresentFactory re-factored to work better with the farm manager's inventory.

--- EasterFarm/EasterFarm.Models/FarmManager.cs
+++ EasterFarm/EasterFarm.Models/FarmManager.cs
@@ -1,11 +1,15 @@
 ﻿namespace EasterFarm.Models
 {
-    using EasterFarm.Models.Market;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+
+    using EasterFarm.Common;
+    using EasterFarm.Models.Contracts;
+    using EasterFarm.Models.Market;
+    using EasterFarm.Models.Presents;
 
     // The player
     public class FarmManager
@@ -16,14 +20,14 @@
         // collect eggs
         // milks the lamb
 
-        private Dictionary<IngredientType, int> inventory;
+        private Dictionary<Enum, int> inventory;
         
         public FarmManager()
         {
-            this.Inventory = new Dictionary<IngredientType, int>();
+            this.Inventory = new Dictionary<Enum, int>();
         }
 
-        public Dictionary<IngredientType, int> Inventory
+        public Dictionary<Enum, int> Inventory
         {
             get
             {
@@ -34,6 +38,29 @@
             {
                 this.inventory = value;
             }
+        }
+
+        public void MakePresent(PresentType presentType, Dictionary<Enum, int> ingredients, PresentFactory presentFactory)
+        {
+            foreach (var ingredient in ingredients)
+            {
+                if (this.Inventory.ContainsKey(ingredient.Key) && this.Inventory[ingredient.Key] >= ingredient.Value)
+                {
+                    this.Inventory[ingredient.Key] -= ingredient.Value;
+                }
+                else
+                {
+                    throw new InsufficientAmmountException(string.Empty, ingredient.Key.ToString());
+                }
+            }
+
+            presentFactory.Get(presentType);
+            this.AddToInventory(presentType);
+        }
+
+        public void AddToInventory(Enum type)
+        {
+            this.Inventory.Add(type, 1);
         }
     }
 }
