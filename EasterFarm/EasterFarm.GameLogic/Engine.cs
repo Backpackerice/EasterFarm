3b8f7d48b124e4c42dccc1c5481f530a0648d7a1
SimoPrG <simeon_petev@hotmail.com>
2015-03-25 13:44:32 +0200

    GameInitializator and Engine in progress

--- EasterFarm/EasterFarm.GameLogic/Engine.cs
+++ EasterFarm/EasterFarm.GameLogic/Engine.cs
@@ -4,13 +4,48 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
+    using System.Threading;
+
+    using Contracts;
+    using EasterFarm.Models;
+    using EasterFarm.Models.Contracts;
 
     public class Engine
     {
-        public void Run()
+        private IRenderer renderer;
+        private IUserInput userInput;
+
+        private List<GameObject> gameObjects;
+
+        public Engine(IRenderer renderer, IUserInput userInput)
         {
-            throw new NotImplementedException();
+            this.renderer = renderer;
+            this.userInput = userInput;
+            this.gameObjects = new List<GameObject>();
+        }
+
+        public void AddGameObject(GameObject gameObject)
+        {
+            this.gameObjects.Add(gameObject);
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                this.renderer.RenderAll();
+
+                Thread.Sleep(200);
+
+                this.userInput.ProcessInput();
+
+                this.renderer.ClearRenderer();
+
+                foreach (var gameObject in gameObjects)
+                {
+                    this.renderer.EnqueueForRendering(gameObject);
+                }
+            }
         }
     }
 }
