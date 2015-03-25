a5b9b5dc8b608a28e6ba1527a4ae7f00674961de
dobomiraboycheva <dobromira.boycheva@gmail.com>
2015-03-25 15:33:15 +0200

    Implement name and price in class Product

--- EasterFarm/EasterFarm.Models/Market/Market.cs
+++ EasterFarm/EasterFarm.Models/Market/Market.cs
@@ -6,7 +6,37 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class Market
+    public class Market:Product
     {
+        private IDictionary<string, decimal> productPrice;
+        
+        public Market(IngredientType name)
+            :base( name)
+        {
+
+        }
+        public decimal QuantityToBuy { get; set; }
+      
+        public decimal CalculateEveryCost()
+        {
+            return ((decimal)this.QuantityToBuy*base.Price);
+        }
+
+        
+        public decimal CalculateFullCost()
+        {
+            //TODO: total cost in market
+            return 0;
+        }
+        public override string ToString()
+        {
+            return string.Format("Total cost:{0,6:c}",CalculateFullCost());
+        }
+
+
+
+
+
+     
     }
 }
--- EasterFarm/EasterFarm.Models/Market/Product.cs
+++ EasterFarm/EasterFarm.Models/Market/Product.cs
@@ -10,9 +10,88 @@
 
     public abstract class Product : IBuyable, IStorable
     {
-        public Enum Type
+
+        private IngredientType name;
+        private int quantity;
+        private decimal price;
+
+       public Product(IngredientType name)
         {
-            get { throw new NotImplementedException(); }
+            this.Name = name;
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+
+       public IngredientType Name { get; set; }
+
+        public int Quantity
+        {
+            get
+            {
+                return 100;
+            }
+            private set
+            {
+              
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                if (Name == IngredientType.Flour)
+                {
+                    return 2;
+                }
+                else if (Name == IngredientType.Cocoa)
+                {
+                    return 4;
+                }
+                else if (Name == IngredientType.Ribbon)
+                {
+                    return 8;
+                }
+                else if (Name == IngredientType.Basket)
+                {
+                    return 12;
+                }
+                else if (Name == IngredientType.Berries)
+                {
+                    return 1;
+                }
+                else if (Name == IngredientType.Rabbit)
+                {
+                    return 15;
+                }
+                else if (Name == IngredientType.Hen)
+                {
+                    return 25;
+                }
+                else if (Name == IngredientType.Milk)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+
+            private set
+            {
+
+            }
+        }
+
+        public abstract decimal CalculateEveryCost();
+       
+
+        public override string ToString()
+        {
+            return string.Format("{0,-25} {1,6:c}", Name, Price);
+
+      
         }
     }
 }
